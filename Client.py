4b7b5f6b6dbc8627fc06cab9731ffdd74db3496a
KaiyWhiteSnow <106684837+KaiyWhiteSnow@users.noreply.github.com>
2023-02-05 16:04:22 +0100

    Working version (VPN hamachi needed)

--- Client.py
+++ Client.py
@@ -1,26 +1,47 @@
 import socket
-import sys
-import time
+import random
+from threading import Thread
+from datetime import datetime
 
-socket_server = socket.socket()
-server_host = socket.gethostname()
-ip = socket.gethostbyname(server_host)
-sport = 8080
- 
-print('This is your IP address: ',ip)
-server_host = input('Enter friend\'s IP address:')
-name = input('Enter Friend\'s name: ')
- 
- 
-socket_server.connect((server_host, sport))
- 
-socket_server.send(name.encode())
-server_name = socket_server.recv(1024)
-server_name = server_name.decode()
- 
-print(server_name,' has joined...')
+# server's IP address
+# if the server is not on this machine, 
+# put the private (network) IP address (e.g 192.168.1.2)
+SERVER_HOST = "25.64.67.85"
+SERVER_PORT = 5002 # server's port
+separator_token = "<SEP>" # we will use this to separate the client name & message
+
+# initialize TCP socket
+s = socket.socket()
+print(f"[*] Connecting to {SERVER_HOST}:{SERVER_PORT}...")
+# connect to the server
+s.connect((SERVER_HOST, SERVER_PORT))
+print("[+] Connected.")
+# prompt the client for a name
+name = input("Enter your name: ")
+
+def listen_for_messages():
+    while True:
+        message = s.recv(1024).decode()
+        print("\n" + message)
+
+# make a thread that listens for messages to this client & print them
+t = Thread(target=listen_for_messages)
+# make the thread daemon so it ends whenever the main thread ends
+t.daemon = True
+# start the thread
+t.start()
+
 while True:
-    message = (socket_server.recv(1024)).decode()
-    print(server_name, ":", message)
-    message = input("Me : ")
-    socket_server.send(message.encode())  
+    # input message we want to send to the server
+    to_send =  input()
+    # a way to exit the program
+    if to_send.lower() == 'q':
+        break
+    # add the datetime, name & the color of the sender
+    date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
+    to_send = f"[{date_now}] {name}{separator_token}{to_send}"
+    # finally, send the message
+    s.send(to_send.encode())
+
+# close the socket
+s.close()
